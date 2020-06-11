0c8cc5636cbc1585db8c1f4fbd3bdff93a05ded4
imfapulousxd <46444631+mytiesto@users.noreply.github.com>
2020-06-11 17:56:40 +0100

    Component

--- Angular/models/questionario.ts
+++ Angular/models/questionario.ts
@@ -1,5 +1,6 @@
 export class Questionario {
-    nome: String;
-    descricao: String;
-    perguntas: [String];
-  }
+  _id: string;
+  nome: String;
+  descricao: String;
+  perguntas: [String];
+}
--- Angular/src/app/questionario/questionario.component.ts
+++ Angular/src/app/questionario/questionario.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { FormBuilder, FormArray, FormControl, Validators, FormGroup } from '@angular/forms';
+import { QuestionarioService } from '../services/questionario.service';
+import { Questionario } from 'models/questionario';
 
 @Component({
   selector: 'app-questionario',
@@ -8,12 +10,19 @@ import { FormBuilder, FormArray, FormControl, Validators, FormGroup } from '@ang
   styleUrls: ['./questionario.component.css']
 })
 export class QuestionarioComponent implements OnInit {
+  questionarios: Array<Questionario>;
 
-  constructor(public _fb: FormBuilder, private userService: UserService) { }
+  constructor(public _fb: FormBuilder, private userService: UserService, private questionarioService: QuestionarioService) { }
 
 
 
   ngOnInit(): void {
+    this.questionarioService.getQuestionarios().subscribe((questionarios) => {
+      this.questionarios = questionarios;
+    })
   }
 
+  getQuestionario(index){
+    console.log(this.questionarios[index]);
+  }
 }
