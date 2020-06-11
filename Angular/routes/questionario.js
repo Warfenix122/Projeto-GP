3c48dfb5a2a85b8a9d242d733e9b574ac10c27ae
imfapulousxd <46444631+mytiesto@users.noreply.github.com>
2020-06-11 17:38:38 +0100

    Nice gralha

--- Angular/routes/questionario.js
+++ Angular/routes/questionario.js
@@ -1,19 +1,19 @@
 const express = require("express");
-const router = express.Router();
 const mongoose = require('mongoose');
+var router = express.Router();
 const Questionario = require('../models/mongoConnection').Questionario;
 
-router.get('/:id'), (req, res) => {
+router.get('/:id', (req, res) => {
     let questionarioId = mongoose.Types.ObjectId(req.params.id);
     Questionario.findOne({_id : questionarioId}).then((questionario) => {
         res.json(questionario);
     })
-}
+})
 
-router.get(''), (req, res) => {
+router.get('', (req, res) => {
     Questionario.find({}).then((questionarios) => {
         res.json(questionarios);
     })
-}
+})
 
 module.exports = router;
