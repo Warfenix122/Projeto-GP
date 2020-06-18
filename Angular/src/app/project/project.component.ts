8583b48d627b587f97bb1adf4e7027cf6b7f50da
patriciaGuerreiro98 <patriciaGuerreiro98@users.noreply.github.com>
2020-06-18 21:26:07 +0100

    cenas e tal

--- Angular/src/app/project/project.component.ts
+++ Angular/src/app/project/project.component.ts
@@ -2,18 +2,22 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from 'models/projeto';
 import { ProjectService } from '../services/project.service';
+import { DatePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+  providers: [DatePipe]
 })
 export class ProjectComponent implements OnInit {
 
   project: Project;
   id: string;
+  date : string;
 
-  constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
+  constructor(private route: ActivatedRoute, private projectService: ProjectService, public datepipe: DatePipe) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
@@ -22,6 +26,8 @@ export class ProjectComponent implements OnInit {
     });
     this.projectService.getProject(this.id).subscribe(project => {
       this.project = project;
+     
+      console.log(this.date);
     });
   }
 
